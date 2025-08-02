24c200b7e5c051919960362b6ee2af44bf601223
AlonsoMoctezuma <moctezuma.alonso98@gmail.com>
2025-08-02 17:47:36 -0600

    parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 const mock_messages = [
-  { text: "Hola", sender: "user"},
-  { text: "Hola, en que te puedo ayudar hoy?", sender: "LLM"},
-  { text: "Quiero que me digas como programar mejor", sender: "user"},
-  { text: "Claro, sigue éstos consejos: puedes usar JavaScript, Python o C++, dependiendo de tu nivel de programación", sender: "LLM"}
+  { content: "Hola", role: "user" },
+  { content: "Hola, en que te puedo ayudar hoy?", role: "assistant" },
+  { content: "Quiero que me digas como programar mejor", role: "user" },
+  { content: "Claro, sigue éstos consejos: puedes usar JavaScript, Python o C++, dependiendo de tu nivel de programación", role: "assistant" }
 ]
- 
+
+
 function App() {
   const [input, setInput] = useState('')
   const [messages, setMessages] = useState(mock_messages)
 
-  function sendMessage() {
+  async function sendMessage() {
     console.log("Enviando mensaje", input)
 
     /* Paso por paso...
@@ -23,20 +23,38 @@ function App() {
     new_messages.push(new_message)
     setMessages(new_messages)
     */
-    
-    setMessages((prev) => [...prev, { text: input, sender: "user" }])
 
-    // Simulando la respuesta
-    setTimeout(() => {
-      setMessages((prev) => [...prev, { text: "Lo siento, hubo un problema con el servidor", sender: "LLM" }])
-    },1000)
+    setMessages((prev) => [...prev, { content: input, role: "user" }])
+
+    // fetch
+
+    // axios
+
+    const data = {
+      "model": "deepseek-r1:1.5b",
+      "stream": false,
+      "think": false,
+      "raw": true,
+      "messages": [
+        {
+          "role": "user",
+          "content": input
+        }
+      ]
+    };
+
+    const respuesta = await axios.post("http://localhost:11434/api/chat", data)
+
+    console.log(respuesta.data.message)
+
+    setMessages((prev) => [...prev, respuesta.data.message])
   }
 
   return (
     <>
 
       <div>
-        {messages.map((message,i) => <p key={i} style={message.sender == 'user' ? {color: 'blue'} : {color: 'red'}}>{message.text}</p>)}
+        {messages.map((message, i) => <p key={i} style={message.role == 'user' ? { color: 'blue' } : { color: 'red' }}>{message.content}</p>)}
       </div>
 
       <input onChange={(e) => setInput(e.target.value)} />
@@ -44,4 +62,5 @@ function App() {
     </>
   )
 }
+
 export default App
